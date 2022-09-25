2ba1ff095ee9c610894c93248e13c0e1393833bc
Beltz Anhxton <beltz.18kyodai@gmail.com>
2022-09-25 16:53:02 -0400

    Sidebar almost Ready and added Theme palette bottom menu

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,4 +1,14 @@
 import '../../Styles/Sidebar.scss'
+import {
+  HomeRounded,
+  AccountCircle,
+  CalendarMonth,
+  Cake,
+  Settings,
+  ExitToApp,
+  Splitscreen,
+  Palette
+} from '@mui/icons-material'
 
 function Sidebar() {
   return (
@@ -9,18 +19,40 @@ function Sidebar() {
       <div className='center'>
         <ul>
           <li>
+            <HomeRounded className='icon' />
             <span>Home</span>
           </li>
           <li>
+            <Splitscreen className='icon' />
+            <span>Tasks</span>
+          </li>
+          <li>
+            <AccountCircle className='icon' />
             <span>Profile</span>
           </li>
           <li>
+            <CalendarMonth className='icon' />
             <span>Calendar</span>
+          </li>
+          <li>
+            <Cake className='icon' />
+            <span>Battleground</span>
+          </li>
+          <li>
+            <Settings className='icon' />
+            <span>Settings</span>
+          </li>
+          <li>
+            <ExitToApp className='icon' />
+            <span>Logout</span>
           </li>
         </ul>
       </div>
-      <div className='bottom'>
-        options
+      <div className="bottom">
+        <span>Theme</span>
+        <div className="colorOption" title='Light'></div>
+        <div className="colorOption" title='Blue'></div>
+        <div className="colorOption" title='Dark'></div>
       </div>
     </div>
   )
