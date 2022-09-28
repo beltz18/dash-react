3fbbdc1aee19c1bee2b4715833d75842342a3cc1
Beltz Anhxton <beltz.18kyodai@gmail.com>
2022-09-28 10:59:35 -0400

    added featuredsChart with circular progressbar and day,week,month stats

--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -1,13 +1,15 @@
 import "../../Styles/Features.scss"
 import {
   MoreVertRounded,
+  KeyboardArrowDown,
+  KeyboardArrowUp,
 } from "@mui/icons-material"
 import { CircularProgressbar } from "react-circular-progressbar"
 import "react-circular-progressbar/dist/styles.css"
 
 const Features = () => {
   return (
-    <div className="features">
+    <div className="features_main">
       <div className="top">
         <h1 className="title">Revenue</h1>
         <MoreVertRounded />
@@ -17,13 +19,48 @@ const Features = () => {
           <CircularProgressbar
             value={70}
             text={"70%"}
-            strokeWidth={1}
+            strokeWidth={2}
           />
-          <p className="title">Total sales made today</p>
-          <p className="amount">$420</p>
-          <p className="desc">
-            Previous transactions processing. Last payment may not be included
-          </p>
+        </div>
+        <p className="title">Total sales made today</p>
+        <p className="amount">$420</p>
+        <p className="description">
+          Previous transactions processing. Last payment may not be included
+        </p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">
+              Today
+            </div>
+            <div className="itemResult positive">
+              <KeyboardArrowUp fontSize="small" />
+              <div className="resultAmount">
+                $12.4k
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">
+              Last week
+            </div>
+            <div className="itemResult negative">
+              <KeyboardArrowDown fontSize="small" />
+              <div className="resultAmount">
+                $12.4k
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">
+              Last Month
+            </div>
+            <div className="itemResult positive">
+              <KeyboardArrowUp fontSize="small" />
+              <div className="resultAmount">
+                $12.4k
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
